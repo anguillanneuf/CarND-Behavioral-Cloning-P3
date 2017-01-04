5688d91399b0c4fe51507add7758b678db0ae2ac
anguillanneuf <tianzih@outlook.com>
2017-01-04 14:37:26 -0500

    added flags so I can reprocess the data using different h, w, ε

--- preprocessing.py
+++ preprocessing.py
@@ -5,62 +5,75 @@ Created on Fri Dec 30 17:08:31 2016
 
 @author: tz
 """
-
 import numpy as np; import pandas as pd; import os; import pickle
-import cv2; # import matplotlib.pyplot as plt
+import cv2; import tensorflow as tf; # import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-raw = pd.read_csv('./data/driving_log.csv')
-for i in raw.columns:
-    if isinstance(raw[i][1], str):     
-        raw[i]=raw[i].map(str.strip)
+flags = tf.app.flags
+FLAGS = flags.FLAGS
 
-tot = raw.shape[0]*3*2
-h = 32; w = 64; c = 3; s = [1,h,w,3]; ε = .25
+# command line flags
+flags.DEFINE_integer('height', 32, "Resize image height")
+flags.DEFINE_integer('width', 64, "Reisze image width")
+flags.DEFINE_float('epsilon', 0.25, "Adjust left/right images by ε")
 
-dat = {'features':np.zeros(shape=[tot,h,w,3]), 'labels':np.zeros(shape=tot), 
-       'position': ['' for i in range(tot)], 
-       'notes': ['org' for i in range(int(tot/2))]+
-                ['aug' for i in range(int(tot/2))]}
+def main(_):
 
-for i, j in enumerate(os.listdir('./data/IMG')):
-    print(j)
-    # original/flipped images.
-    img = cv2.cvtColor(cv2.imread('./data/IMG/'+j.strip()), cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (w,h))
-    flipped = cv2.flip(img,1)
+    raw = pd.read_csv('./data/driving_log.csv')
+    for i in raw.columns:
+        if isinstance(raw[i][1], str):
+            raw[i]=raw[i].map(str.strip)
     
-    # adjusted angles for left/right images/new angles for flipped images
-    pos_old = ('left' if j.find('left')>=0 else 
-               'right' if j.find('right')>=0 else 'center')    
-    pos_new = ('right' if j.find('left')>=0 else 
-               'left' if j.find('right')>=0 else 'center')
+    tot = raw.shape[0]*3*2
+    h = FLAGS.height; w = FLAGS.width; ε = FLAGS.epsilon
     
-    old = raw[raw[pos_old]=='IMG/'+j]['steering']
-    adjusted = np.clip(old+ε if pos_old=='left' else 
-                       old-ε if pos_old=='right' else old, -1., 1.)
-    new = adjusted*-1.
+    dat = {'features':np.zeros(shape=[tot,h,w,3]), 'labels':np.zeros(shape=tot), 
+           'position': ['' for i in range(tot)], 
+           'notes': ['org' for i in range(int(tot/2))]+
+                    ['aug' for i in range(int(tot/2))]}
     
-    dat['features'][i] = img
-    dat['labels'][i] = adjusted
-    dat['position'][i] = pos_old
-    dat['features'][i+int(tot/2)] = flipped
-    dat['labels'][i+int(tot/2)] = new
-    dat['position'][i+int(tot/2)] = pos_new
+    for i, j in enumerate(os.listdir('./data/IMG')):
+        print(j)
+        # original/flipped images.
+        img = cv2.cvtColor(cv2.imread('./data/IMG/'+j.strip()), cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (w,h))
+        flipped = cv2.flip(img,1)
+        
+        # adjusted angles for left/right images/new angles for flipped images
+        pos_old = ('left' if j.find('left')>=0 else 
+                   'right' if j.find('right')>=0 else 'center')    
+        pos_new = ('right' if j.find('left')>=0 else 
+                   'left' if j.find('right')>=0 else 'center')
+        
+        old = raw[raw[pos_old]=='IMG/'+j]['steering']
+        adjusted = np.clip(old+ε if pos_old=='left' else 
+                           old-ε if pos_old=='right' else old, -1., 1.)
+        new = adjusted*-1.
+        
+        dat['features'][i] = img
+        dat['labels'][i] = adjusted
+        dat['position'][i] = pos_old
+        dat['features'][i+int(tot/2)] = flipped
+        dat['labels'][i+int(tot/2)] = new
+        dat['position'][i+int(tot/2)] = pos_new
+    
+    X_train, X_test, y_train, y_test = \
+    train_test_split(dat['features'], dat['labels'], test_size=0.2)
+    
+    X_train, X_val, y_train, y_val = \
+    train_test_split(X_train, y_train, test_size=0.2)
+    
+    train = {'features': X_train, 'labels': y_train}
+    validation = {'features': X_val, 'labels': y_val}
+    test = {'features': X_test, 'labels': y_test}
+    
+    with open("./data/train.p", "wb") as f:
+        pickle.dump(train, f)
+    with open("./data/validation.p", "wb") as f:
+        pickle.dump(validation, f)
+    with open("./data/test.p", "wb") as f:
+        pickle.dump(test, f)
 
-X_train, X_test, y_train, y_test = \
-train_test_split(dat['features'], dat['labels'], test_size=0.2)
-
-X_train, X_val, y_train, y_val = \
-train_test_split(X_train, y_train, test_size=0.2)
-
-train = {'features': X_train, 'labels': y_train}
-validation = {'features': X_val, 'labels': y_val}
-test = {'features': X_test, 'labels': y_test}
-
-with open("./data/train.p", "wb") as f:
-    pickle.dump(train, f)
-with open("./data/validation.p", "wb") as f:
-    pickle.dump(validation, f)
-with open("./data/test.p", "wb") as f:
-    pickle.dump(test, f)
+# parses flags and calls the `main` function above
+if __name__ == '__main__':
+    tf.app.run()
