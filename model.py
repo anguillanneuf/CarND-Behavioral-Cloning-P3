5fc15df1e54f0d6527f2bf41fbd5a5b92d458120
anguillanneuf <tianzih@outlook.com>
2017-01-04 12:01:02 -0500

    pre-processing script looks good now.

--- model.py
+++ model.py
@@ -7,38 +7,55 @@ Created on Fri Dec 30 17:08:31 2016
 """
 
 import numpy as np; import pandas as pd; import os; import pickle
-import cv2
+import cv2; # import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 raw = pd.read_csv('./data/driving_log.csv')
-h = 160; w = 320; c = 3; s = [1,h,w,3]
-ε = .25
-dat = {'features':np.zeros(shape=s), 'labels':np.array([0]), 
-       'notes': ['unk'], 'position'; ['unk']}
 
-for j in os.listdir('./data/IMG')[:100]:
-    
-    img = cv2.cvtColor(cv2.imread('./data/IMG/' + j.strip()), 
-                       cv2.COLOR_BGR2RGB)
+tot = raw.shape[0]*2
+h = 64; w = 128; c = 3; s = [1,h,w,3]; ε = .25
+
+dat = {'features':np.zeros(shape=[tot,h,w,3]), 'labels':np.zeros(shape=tot), 
+       'position': ['' for i in range(tot)], 
+       'notes': ['org' for i in range(int(tot/2))]+['aug' for i in range(tot)]}
+
+for i, j in enumerate(os.listdir('./data/IMG')):
+    # original/flipped images.
+    img = cv2.cvtColor(cv2.imread('./data/IMG/'+j.strip()), cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (w,h))
     flipped = cv2.flip(img,1)
     
-    pos = ('left' if j.find('left')>=0 else 
-           'right' if j.find('right')>=0 else 'center')
+    # adjusted angles for left/right images/new angles for flipped images
+    pos_old = ('left' if j.find('left')>=0 else 
+               'right' if j.find('right')>=0 else 'center')    
+    pos_new = ('right' if j.find('left')>=0 else 
+               'left' if j.find('right')>=0 else 'center')
     
-    old = raw[raw[pos]=='IMG/'+j]['steering']
-    old_adjusted = np.clip(old+ε if pos=='left' else 
-                           old-ε if pos=='right' else old, -1, 1)
-    new = old_adjusted*-1
+    old = raw[raw[pos_old]=='IMG/'+j]['steering']
+    adjusted = np.clip(old+ε if pos_old=='left' else 
+                       old-ε if pos_old=='right' else old, -1., 1.)
+    new = adjusted*-1.
     
-    dat['features']=np.append(dat['features'], img.reshape(s), axis=0)
-    dat['labels']=np.append(dat['labels'], old_adjusted)
-    dat['notes'].append('original')
-    dat['position'].append(pos)
-    dat['features']=np.append(dat['features'], flipped.reshape(s), axis=0)
-    dat['labels']=np.append(dat['labels'], new)
-    dat['notes'].append('transformed')
-    dat['position'].append(pos)
-    
-with open("./data/dat.p", "wb") as f:
-    pickle.dump(dat, f)
-    
-# validation and test should come from original+center
+    dat['features'][i] = img
+    dat['labels'][i] = adjusted
+    dat['position'][i] = pos_old
+    dat['features'][i+int(tot/2)] = flipped
+    dat['labels'][i+int(tot/2)] = new
+    dat['position'][i+int(tot/2)] = pos_new
+
+X_train, X_test, y_train, y_test = \
+train_test_split(dat['features'], dat['labels'], test_size=0.2)
+
+X_train, X_val, y_train, y_val = \
+train_test_split(X_train, y_train, test_size=0.2)
+
+train = {'features': X_train, 'labels': y_train}
+validation = {'features': X_val, 'labels': y_val}
+test = {'features': X_test, 'labels': y_test}
+
+with open("./data/train.p", "wb") as f:
+    pickle.dump(train, f)
+with open("./data/validation.p", "wb") as f:
+    pickle.dump(validation, f)
+with open("./data/test.p", "wb") as f:
+    pickle.dump(test, f)
